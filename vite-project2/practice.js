4f6840651b8e4ee74b7fc7788b3c10df2a279818
xuuwon <dumpling1011@gmail.com>
2024-10-14 10:44:53 +0900

    basic state

---
+++ vite-project2/practice.js
@@ -0,0 +1,37 @@
+const fruits = [
+    { name: "딸기", color: "빨깅", 크기: 2 },
+    { name: "바나나", color: "노랑", 크기: 5 },
+    { name: "키위", color: "초록", 크기: 3 },
+    { name: "사과", color: "빨깅", 크기: 4 },
+    { name: "블루베리", color: "파랑", 크기: 1 },
+    { name: "오렌지", color: "주황", 크기: 4 },
+    { name: "포도", color: "보라", 크기: 2 },
+    { name: "레몬", color: "노랑", 크기: 3 },
+    { name: "체리", color: "빨강", 크기: 2 },
+    { name: "수박", color: "초록", 크기: 8 },
+]
+
+// 과일 이름만 담겨있는 배열 만들기
+fruits.map((fruit) => {
+    return fruit.name;
+})
+
+// 과일 이름에 '주스' 붙인 배열 만들기
+fruits.map((fruit) => {
+    return fruit.name + '주스';
+})
+
+// 과일 색이 빨강인 과일만 남긴 배열 만들기
+fruits.filter((fruit) => {
+    return fruit.color === '빨강';
+})
+
+// 과일 크기가 3 이상인 과일만 남긴 배열 만들기
+fruits.filter((fruit) => {
+    return fruit.크기 >= 3;
+})
+
+// 과일 색이 빨강이면서 크기가 3 이상인 과일의 이름만 남긴 배열 만들기
+fruits.filter((fruit) => {
+    fruit.크기 >= 3 && fruit.color === '빨강'
+}).map((fruit) => fruit.name);
--- vite-project2/src/App.jsx
+++ vite-project2/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import { useState } from 'react';
 import './App.css'
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [counter, setCounter] = useState(0); 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//   return (
+//     <>
+//       <div>counter : {counter}</div>
+//       <button 
+//         onClick={() => {
+//           setCounter((prev) => prev + 1)
+//         }}
+//       >+</button>
+
+//       <button 
+//         onClick={() => {
+//           setCounter((prev) => prev + -1)
+//         }}
+//       >-</button>
+//     </>);
+// }
+
+class App extends Component {
+  state = { counter: 2 }
+
+  render() {
+    return (
+      <>
+        <div>counter {this.state.counter}</div>
+        <button 
+          onClick={() => this.setState({ counter: this.state.counter + 1})}
+        >+</button>
+        <button 
+          onClick={() => this.setState({ counter: this.state.counter - 1})}
+        >-</button>
+      </>
+    )
+  }
 }
 
-export default App
+export default App;
