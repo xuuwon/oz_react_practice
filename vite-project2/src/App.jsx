0b90377b46a3e7ddde91a4ed0d14ea480829b750
xuuwon <dumpling1011@gmail.com>
2024-10-16 11:55:31 +0900

    routing practice

--- vite-project2/src/App.jsx
+++ vite-project2/src/App.jsx
@@ -3,54 +3,59 @@
 import { useState } from 'react';
 import './App.css'
 import { Component } from 'react';
+import { Link, Routes, Route, useNavigate, useLocation, useParams, useSearchParams } from 'react-router-dom';
 
 function App() {
-  const [mood, setMood] = useState("Normal");
+  const navigate = useNavigate();
+  // navigate( ? )
+
+  const location = useLocation();
 
   return (
     <>
-      <Face3 mood={mood} />
-      <div className={mood === "Happy" ? 'happy' : mood === "Normal" ? 'normal' : 'sad'}>기분: {mood}</div>
       <div>
-        <button onClick={() => setMood("Happy")}>HAPPY</button>            
-        <button onClick={() => setMood("Normal")}>NORMAL</button>
-        <button onClick={() => setMood("Sad")}>SAD</button>
+        {/* <Link to="/main">메인</Link>
+        <Link to="/mypage"> 마이페이지</Link>
+        <Link to="/test"> 테스트</Link> */}
+
+        <div>
+          <button onClick={() => navigate('/main')}>메인</button>
+          <button onClick={() => navigate('/mypage')}>마이페이지</button>
+          <button onClick={() => navigate('/test')}>테스트</button>
+        </div>
+
+        <div>
+          <button onClick={() => navigate(1)}>{`앞으로 가기 ->`}</button>
+          <button onClick={() => navigate(-1)}>{`<- 뒤로 가기`}</button>
+        </div>
       </div>
+      
+      <Routes>
+        <Route path="/main" element={<Main />}/>
+        <Route path="/mypage" element={<div>마이 페이지</div>}/>
+        <Route path="/test" element={<div>테스트 페이지</div>}/>
+      </Routes>
     </>);
 }
 
-// 1. if문으로 리턴하는 JSX문 바꿔주기
-function Face({ mood }) {
-  if (mood === "Happy") {
-    return <div>😆</div>
-  } else if (mood === "Normal") {
-    return <div>🙂</div>
-  } else {
-    return <div>😭</div> 
-  }
+function Main() {
+  const params = useParams();
+  console.log(params.name);
+
+  const location = useLocation();
+  console.log(location.search); // 물음표 뒤 문자열
+  // http://localhost:5174/main?name=yuwon&school=ozcoding
+  // -> ?name=yuwon&school=ozcoding
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  // 물음표 뒤 문자열에서 해당하는 값만 빼올 수 있음
+  console.log(searchParams.get('name'));
+  // -> yuwon
+  console.log(searchParams.get('school'));
+  // -> ozcoding
+
+  return <div>메인 페이지</div>;
 }
 
-// 2. 삼항 연산자 사용하기
-function Face2({ mood }) {
-  return (
-    <>
-      {mood === "Happy" ? <div>😆</div>
-      : mood === "Normal" ? <div>🙂</div>
-      : <div>😭</div> 
-      }
-    </>
-  )
-}
-
-// 3. 논리 연산자
-function Face3({ mood }) {
-  return (
-    <>
-      {mood === "Happy" && <div>😆</div>}
-      {mood === "Normal" && <div>🙂</div>}
-      {mood === "Sad" && <div>😭</div>}
-    </>
-  )
-}
 
 export default App;
