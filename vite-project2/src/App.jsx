1e6e20d4d1bb3a9d95f764cc067f8b56615b8fd5
xuuwon <dumpling1011@gmail.com>
2024-10-18 11:45:25 +0900

    life cycle function

--- vite-project2/src/App.jsx
+++ vite-project2/src/App.jsx
@@ -6,61 +6,108 @@ import Search from "./page/Search";
 import Main from "./page/Main";
 
 function App() {
-  const [showCounter, setShowCounter] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []) // 처음 불러올 때만 데이터를 받아오도록 빈 배열 추가
 
   return(
     <>
-      {showCounter && <Counter />}
-      <br />
-      <button onClick={() => setShowCounter(!showCounter)}>show?</button>
+      <h1>데이터 목록</h1>
+      {data.map(el => <div key={el.id}>{el.content}</div>)}
+      <MouseFollower />
+      <ScrollIndicator />
+      <AlertTimer />
+      <div style={{height: "300vh"}}></div>
     </>
   )
 }
 
-function Counter() {
-  const [counter, setCounter] = useState(1);
-  const [counter2, setCounter2] = useState(100);
+const MouseFollower = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0});
 
-  // 1. 컴포넌트가 최초로 렌더링 될 때만 조작 하고 싶다.
   useEffect(() => {
-    console.log("맨 처음 렌더링 될 때")
-  }, [])
-
-  // 2. 컴포넌트가 리렌더링 될 때 조작하고 싶다.
-  useEffect(() => {
-    console.log("리렌더링...")
-  })
-
-  // 3. 특정 상태값이 변할 때에만 조작하고 싶다.
-  useEffect(() => {
-    console.log("counter의 값이 변할 때")
-  }, [counter])
-
-  // 4. 컴포넌트가 최종적으로 언마운트 될 때 조작하고 싶다.
-  useEffect(() => {
-    return () => {
-      console.log("컴포넌트 언마운트");
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY })
     }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => { // cleanup 함수
+      window.removeEventListener('mousemove', handleMouseMove)
+    };
   })
-
-  useEffect(() => {
-    console.log("useEffect");
-
-    return () => {
-      console.log("returned function");
-    }
-  }, [counter2]); // counter2가 변경될 때마다 useEffect 실행
-  // 리 렌더링 되면 이전 컴포넌트는 사라지기 때문에 return된 함수도 호출됨.
-
 
   return (
-    <>
-      <div>counter: {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+1</button>
-      <div>counter2: {counter2}</div>
-      <button onClick={() => setCounter2(counter2 - 1)}>-1</button>
-    </>
+    <div style={{
+      position: "fixed", 
+      top: position.y,
+      left: position.x,
+      width: "20px",
+      height: "20px",
+      borderRadius: "50%",
+      backgroundColor: "blue",
+      transform: "translate(-50%, -50%)",
+      pointerEvents: "none" // 다른 버튼 클릭 이벤트를 막지 않도록
+    }}></div>
   )
 }
+
+
+const ScrollIndicator = () => {
+  const [scrollWidth, setScrollWidth] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const windowHeight = 
+      document.documentElement.scrollHeight
+      - document.documentElement.clientHeight
+
+      const scrollPercentage = (scrollTop / windowHeight) * 100;
+
+      setScrollWidth(scrollPercentage);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  })
+
+  return (
+    <div style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: `${scrollWidth}%`,
+      height: "10px",
+      backgroundColor: "blue"
+    }}></div>
+  )
+}
+
+const AlertTimer = () => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    const setTimeoutId = setTimeout(() => {
+      showAlert === true ? alert('시간 초과') : null
+    }, 3000)
+
+    return () => {
+      clearTimeout(setTimeoutId);
+    }
+  })
+
+  return (
+    <button onClick={() => setShowAlert(false)}>빨리 클릭!!!</button>
+  )
+}
+
 
 export default App;
